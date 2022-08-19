9e4bbc4192816a4f9a15261247b5ed657c1faaa3
Patrycioss <72610925+Patrycioss@users.noreply.github.com>
2022-08-19 17:14:34 +0200

    Added a register that registers when players drop items to prevent dropping the EmpireWand from triggering the ability

--- src/main/java/me/patrycioss/empirewand/EmpireWandListener.kt
+++ src/main/java/me/patrycioss/empirewand/EmpireWandListener.kt
@@ -4,16 +4,21 @@ import me.patrycioss.empirewand.abilities.AbilityManager
 import me.patrycioss.empirewand.commands.EmpireWandCommand
 import org.bukkit.Bukkit
 import org.bukkit.Material
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
+import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.event.player.PlayerInteractEvent
+import java.util.*
 import java.util.logging.Logger
 
 
-class EmpireWandListener(empireWand: EmpireWand) : Listener
+class EmpireWandListener(private val empireWand: EmpireWand) : Listener
 {
     private val logger: Logger = Bukkit.getLogger()
+
+    val playerDropItemOnTick = mutableListOf<UUID>()
 
     init
     {
@@ -23,8 +28,38 @@ class EmpireWandListener(empireWand: EmpireWand) : Listener
     }
 
     @EventHandler
+    fun onItemDrop(dropItemEvent: PlayerDropItemEvent)
+    {
+        registerItemDropper(dropItemEvent.player)
+    }
+
+
+    /**
+     * Registers players which have dropped an item and removes them from the registry after an appropriate amount of time
+     */
+    private fun registerItemDropper(player: Player)
+    {
+        playerDropItemOnTick.add(player.uniqueId)
+
+        Bukkit.getServer().scheduler.runTaskLater(empireWand,
+
+            Runnable
+        {
+            playerDropItemOnTick.remove(player.uniqueId)
+        },
+            2L)
+    }
+
+    /**
+     * Handles general player interactions with the wand
+     */
+    @EventHandler
     fun onUseWand(playerInteractEvent: PlayerInteractEvent)
     {
+        //Make sure the ability doesn't trigger because of dropping items
+        if (playerDropItemOnTick.contains(playerInteractEvent.player.uniqueId)) return
+
+
         if (playerInteractEvent.hasItem() && playerInteractEvent.item!!.type == Material.BLAZE_ROD)
         {
             if (playerInteractEvent.item!!.hasItemMeta() && playerInteractEvent.item!!.itemMeta.hasDisplayName())
@@ -51,3 +86,5 @@ class EmpireWandListener(empireWand: EmpireWand) : Listener
         }
     }
 }
+
+
